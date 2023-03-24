09be99cc2f38da01d6399eaa39f923e9752bcfce
Rahil Mehta <rahilnmehta2000@gmail.com>
2023-03-24 19:28:20 +0530

    minor changes to booking page

--- my-booking.php
+++ my-booking.php
@@ -132,8 +132,8 @@ foreach($results as $result)
                 <div class="vehicle_img"> <a href="vehical-details.php?vhid=<?php echo htmlentities($result->vid);?>"><img src="admin/img/vehicleimages/<?php echo htmlentities($result->Vimage1);?>" alt="image"></a> </div>
                 <div class="vehicle_title">
 
-                  <h6><a href="vehical-details.php?vhid=<?php echo htmlentities($result->vid);?>"> <?php echo htmlentities($result->BrandName);?> , <?php echo htmlentities($result->VehiclesTitle);?></a></h6>
-                  <p><b>From </b> <?php echo htmlentities($result->FromDate);?> <b>To </b> <?php echo htmlentities($result->ToDate);?></p>
+                  <h6><a href="vehical-details.php?vhid=<?php echo htmlentities($result->vid);?>"> <?php //echo htmlentities($result->BrandName);?>  <?php echo htmlentities($result->VehiclesTitle);?></a></h6>
+                  <!-- <p><b>From </b> <?php //echo htmlentities($result->FromDate);?> <b>To </b> <?php //echo htmlentities($result->ToDate);?></p> -->
                   <div style="float: left"><p><b>Message:</b> <?php echo htmlentities($result->message);?> </p></div>
                 </div>
                 <?php if($result->Status==1)
@@ -157,7 +157,7 @@ foreach($results as $result)
        
               </li>
 
-<h5 style="color:blue">Invoice</h5>
+<!-- <h5 style="color:blue">Invoice</h5>
 <table>
   <tr>
     <th>Car Name</th>
@@ -167,17 +167,17 @@ foreach($results as $result)
     <th>Rent / Day</th>
   </tr>
   <tr>
-    <td><?php echo htmlentities($result->VehiclesTitle);?>, <?php echo htmlentities($result->BrandName);?></td>
-     <td><?php echo htmlentities($result->FromDate);?></td>
-      <td> <?php echo htmlentities($result->ToDate);?></td>
-       <td><?php echo htmlentities($tds=$result->totaldays);?></td>
-        <td> <?php echo htmlentities($ppd=$result->PricePerDay);?></td>
+    <td><?php //echo htmlentities($result->VehiclesTitle);?>, <?php echo htmlentities($result->BrandName);?></td>
+     <td><?php //echo htmlentities($result->FromDate);?></td>
+      <td> <?php //echo htmlentities($result->ToDate);?></td>
+       <td><?php //echo htmlentities($tds=$result->totaldays);?></td>
+        <td> <?php //echo htmlentities($ppd=$result->PricePerDay);?></td>
   </tr>
   <tr>
     <th colspan="4" style="text-align:center;"> Grand Total</th>
-    <th><?php echo htmlentities($tds*$ppd);?></th>
+    <th><?php //echo htmlentities($tds*$ppd);?></th>
   </tr>
-</table>
+</table> -->
 <hr />
               <?php }}  else { ?>
                 <h5 align="center" style="color:red">No booking yet</h5>
