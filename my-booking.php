3277e17e84737eb3f97cb43d76483fb994b4210d
Rahil Mehta <rahilnmehta2000@gmail.com>
2023-03-24 17:29:13 +0530

    d

--- my-booking.php
+++ my-booking.php
@@ -41,7 +41,7 @@ else{
 <link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/images/favicon-icon/apple-touch-icon-72-precomposed.png">
 <link rel="apple-touch-icon-precomposed" href="assets/images/favicon-icon/apple-touch-icon-57-precomposed.png">
 <link rel="shortcut icon" href="assets/images/favicon-icon/favicon.png">
-<!-- Google-Font-->
+  <!-- Google-Font-->
 <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet">
 <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
 <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
