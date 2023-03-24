82cd436ba8d1e5a21ef5f27ff24cea6fc441faa6
Rahil Mehta <rahilnmehta2000@gmail.com>
2023-03-25 03:29:20 +0530

    Legal Machines

--- admin/includes/leftbar.php
+++ admin/includes/leftbar.php
@@ -11,14 +11,14 @@
 </ul>
 </li>
 
-<li><a href="#"><i class="fa fa-car"></i> Vehicles</a>
+<li><a href="#"><i class="fa fa-car"></i> Fleet</a>
 					<ul>
 						<li><a href="post-avehical.php">Post a Vehicle</a></li>
 						<li><a href="manage-vehicles.php">Manage Vehicles</a></li>
 					</ul>
 				</li>
 
-<li><a href="#"><i class="fa fa-sitemap"></i> Bookings</a>
+<li><a href="#"><i class="fa fa-sitemap"></i> Test Drive</a>
 					<ul>
 						<li><a href="new-bookings.php">New</a></li>
 						<li><a href="confirmed-bookings.php">Confirmed</a></li>
@@ -30,7 +30,7 @@
 
 				<li><a href="testimonials.php"><i class="fa fa-table"></i> Manage Testimonials</a></li>
 				<!-- <li><a href="manage-conactusquery.php"><i class="fa fa-desktop"></i> Manage Conatctus Query</a></li> -->
-				<li><a href="reg-users.php"><i class="fa fa-users"></i> Reg Users</a></li>
+				<li><a href="reg-users.php"><i class="fa fa-users"></i> Registered Users</a></li>
 			<!-- <li><a href="manage-pages.php"><i class="fa fa-files-o"></i> Manage Pages</a></li> -->
 			<li><a href="update-contactinfo.php"><i class="fa fa-files-o"></i> Update Contact Info</a></li>
 
