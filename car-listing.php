ac0af83cace9b23ea7df35a0aa8874b555e0cf17
Rahil Mehta <rahilnmehta2000@gmail.com>
2023-03-24 18:25:17 +0530

    updates to user side

--- car-listing.php
+++ car-listing.php
@@ -100,8 +100,9 @@ foreach($results as $result)
           <div class="product-listing-img"><img src="admin/img/vehicleimages/<?php echo htmlentities($result->Vimage1);?>" class="img-responsive" alt="Image" /> </a> 
           </div>
           <div class="product-listing-content">
-            <h5><a href="vehical-details.php?vhid=<?php echo htmlentities($result->id);?>"><?php echo htmlentities($result->BrandName);?> , <?php echo htmlentities($result->VehiclesTitle);?></a></h5>
-            <p class="list-price">$<?php echo htmlentities($result->PricePerDay);?> Per Day</p>
+            <h5><a href="vehical-details.php?vhid=<?php echo htmlentities($result->id);?>"><?php //echo htmlentities($result->BrandName);?>  <?php echo htmlentities($result->VehiclesTitle);?></a></h5>
+            
+            <br><!-- <p class="list-price">$<?php //echo htmlentities($result->PricePerDay);?> Per Day</p> -->
             <ul>
               <li><i class="fa fa-user" aria-hidden="true"></i><?php echo htmlentities($result->SeatingCapacity);?> seats</li>
               <li><i class="fa fa-calendar" aria-hidden="true"></i><?php echo htmlentities($result->ModelYear);?> model</li>
@@ -144,7 +145,7 @@ foreach($results as $result)
                   <option>Select Fuel Type</option>
 <option value="Petrol">Petrol</option>
 <option value="Diesel">Diesel</option>
-<option value="CNG">CNG</option>
+<!-- <option value="CNG">CNG</option> -->
                 </select>
               </div>
              
@@ -173,8 +174,8 @@ foreach($results as $result)
 
               <li class="gray-bg">
                 <div class="recent_post_img"> <a href="vehical-details.php?vhid=<?php echo htmlentities($result->id);?>"><img src="admin/img/vehicleimages/<?php echo htmlentities($result->Vimage1);?>" alt="image"></a> </div>
-                <div class="recent_post_title"> <a href="vehical-details.php?vhid=<?php echo htmlentities($result->id);?>"><?php echo htmlentities($result->BrandName);?> , <?php echo htmlentities($result->VehiclesTitle);?></a>
-                  <p class="widget_price">$<?php echo htmlentities($result->PricePerDay);?> Per Day</p>
+                <div class="recent_post_title"> <a href="vehical-details.php?vhid=<?php echo htmlentities($result->id);?>"><?php //echo htmlentities($result->BrandName);?>  <?php echo htmlentities($result->VehiclesTitle);?></a>
+                  <!-- <p class="widget_price">$<?php //echo htmlentities($result->PricePerDay);?> Per Day</p> -->
                 </div>
               </li>
               <?php }} ?>
